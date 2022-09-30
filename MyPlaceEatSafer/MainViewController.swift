4adfced7f807ff879abda73a6fddf1b19f3af6c9
vladislavikys <zarya.vlad@yandex.ru>
2022-09-30 16:26:09 +0300

    test push Xcode

--- MyPlaceEatSafer/MainViewController.swift
+++ MyPlaceEatSafer/MainViewController.swift
@@ -33,6 +33,7 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
         
         return cell
     }
